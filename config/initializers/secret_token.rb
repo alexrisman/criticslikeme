a9acc2cdf798505ebe31cb7bf9129ec93f7eca3e
Yee <yeekiwai@gmail.com>
2013-02-10 18:27:31 -0600

    Initialize rails project

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Belch::Application.config.secret_token = 'eb5a1840f142919148f43ecdc15ef546452d9e618da7bc1a9c9a1b936927283993aaeeb71562d5c3db716a75f7602408d907c9da36408142ad261f244d625f1c'
