89540536685e61c3145267b5c1cb7f65e1371bf3
Yee <yeekiwai@gmail.com>
2013-02-19 11:48:11 -0600

    Use aws s3 to image uploads

--- app/models/beer.rb
+++ app/models/beer.rb
@@ -5,8 +5,18 @@ class Beer < ActiveRecord::Base
   has_many :users, through: :ratings
 
   #photo stuffs
-  
-  has_attached_file :photo
+  # s3 Credentials
+  options = if Rails.env.production? 
+    {:storage => :s3,
+      :bucket => ENV['S3_BUCKET_NAME'],
+      :s3_credentials => {
+       :access_key_id => ENV['AWS_ACCESS_KEY_ID'],
+       :secret_access_key => ENV['AWS_SECRET_ACCESS_KEY']}
+     }
+  else
+     {}  
+  end
+  has_attached_file :photo, options
   validates_attachment_size :photo, :less_than => 2.megabytes
   validates_attachment_content_type :photo, :content_type=>['image/jpeg', 'image/png', 'image/gif']
   
