78d73a25f0af775357e660f24556fbaf7e9121b4
Yee <yeekiwai@gmail.com>
2013-04-30 17:32:52 -0500

    Make banner an editable/updatable attribute. closes #20

--- app/models/event.rb
+++ app/models/event.rb
@@ -1,8 +1,21 @@
 class Event < ActiveRecord::Base
-  attr_accessible :date, :details, :name, :token, :admin_id
+  attr_accessible :date, :details, :name, :token, :admin_id, :banner
   has_many :interests
   has_and_belongs_to_many :users
   belongs_to :admin, :class_name => "User"
+  
+  options = if Rails.env.production? && ENV['S3_BUCKET_NAME']
+    {:storage => :s3,
+      :bucket => ENV['S3_BUCKET_NAME'],
+      :s3_credentials => {
+       :access_key_id => ENV['AWS_ACCESS_KEY_ID'],
+       :secret_access_key => ENV['AWS_SECRET_ACCESS_KEY']},
+       :default_url => "missing.jpg"
+     }
+  else
+     {:default_url => "missing.jpg"}  
+  end
+  has_attached_file :banner, options
   
   before_create { generate_token(:token) }
   def generate_token(column)
--- db/schema.rb
+++ db/schema.rb
@@ -11,16 +11,26 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20130428235235) do
+ActiveRecord::Schema.define(:version => 20130430221918) do
+
+  create_table "conversations", :force => true do |t|
+    t.string   "subject",    :default => ""
+    t.datetime "created_at",                 :null => false
+    t.datetime "updated_at",                 :null => false
+  end
 
   create_table "events", :force => true do |t|
     t.string   "name"
     t.datetime "date"
     t.text     "details"
-    t.datetime "created_at", :null => false
-    t.datetime "updated_at", :null => false
+    t.datetime "created_at",          :null => false
+    t.datetime "updated_at",          :null => false
     t.integer  "admin_id"
     t.string   "token"
+    t.string   "banner_file_name"
+    t.string   "banner_content_type"
+    t.integer  "banner_file_size"
+    t.datetime "banner_updated_at"
   end
 
   create_table "events_users", :id => false, :force => true do |t|
@@ -44,6 +54,26 @@ ActiveRecord::Schema.define(:version => 20130428235235) do
     t.datetime "updated_at",   :null => false
   end
 
+  create_table "notifications", :force => true do |t|
+    t.string   "type"
+    t.text     "body"
+    t.string   "subject",              :default => ""
+    t.integer  "sender_id"
+    t.string   "sender_type"
+    t.integer  "conversation_id"
+    t.boolean  "draft",                :default => false
+    t.datetime "updated_at",                              :null => false
+    t.datetime "created_at",                              :null => false
+    t.integer  "notified_object_id"
+    t.string   "notified_object_type"
+    t.string   "notification_code"
+    t.string   "attachment"
+    t.boolean  "global",               :default => false
+    t.datetime "expires"
+  end
+
+  add_index "notifications", ["conversation_id"], :name => "index_notifications_on_conversation_id"
+
   create_table "ratings", :force => true do |t|
     t.integer  "stars"
     t.datetime "created_at",  :null => false
@@ -52,6 +82,20 @@ ActiveRecord::Schema.define(:version => 20130428235235) do
     t.integer  "interest_id"
     t.text     "review"
   end
+
+  create_table "receipts", :force => true do |t|
+    t.integer  "receiver_id"
+    t.string   "receiver_type"
+    t.integer  "notification_id",                                  :null => false
+    t.boolean  "is_read",                       :default => false
+    t.boolean  "trashed",                       :default => false
+    t.boolean  "deleted",                       :default => false
+    t.string   "mailbox_type",    :limit => 25
+    t.datetime "created_at",                                       :null => false
+    t.datetime "updated_at",                                       :null => false
+  end
+
+  add_index "receipts", ["notification_id"], :name => "index_receipts_on_notification_id"
 
   create_table "users", :force => true do |t|
     t.datetime "created_at",          :null => false
