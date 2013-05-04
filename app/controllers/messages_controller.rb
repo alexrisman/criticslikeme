48b6817e65e6596f3ddd86cb8ec78d37740229b3
Alex Risman <mthmn20@gmail.com>
2013-05-03 21:48:52 -0500

    "Not found" pages for nonexistent records

--- app/controllers/messages_controller.rb
+++ app/controllers/messages_controller.rb
@@ -14,26 +14,30 @@ class MessagesController < ApplicationController
   # GET /messages/1
   # GET /messages/1.json
   def show
-    @user = User.find params[:user_id]
-    if current_user == @user
-      @common_ground = ["Our records indicate that you are in fact you, and what you have in common with you is everything. What an incredible coincidence!"]
+    @user = User.find_by_id(params[:user_id])
+    if !@user
+      render :template => "sessions/notfound"
     else
-      @common_ground = current_user.shared_list @user
-    end
-    @messages = messages_between(current_user, @user)
-    @message = Message.new
-    
-    #Set messages to current_user as read
-    @messages.each do |m| 
-      if m.recipient == current_user && m.is_read == false
-        m.is_read = true;
-        m.save!
+      if current_user == @user
+        @common_ground = ["Our records indicate that you are in fact you, and what you have in common with you is everything. What an incredible coincidence!"]
+      else
+        @common_ground = current_user.shared_list @user
       end
-    end
+      @messages = messages_between(current_user, @user)
+      @message = Message.new
+      
+      #Set messages to current_user as read
+      @messages.each do |m| 
+        if m.recipient == current_user && m.is_read == false
+          m.is_read = true;
+          m.save!
+        end
+      end
 
-    respond_to do |format|
-      format.html # show.html.erb
-      format.json { render json: @message }
+      respond_to do |format|
+        format.html # show.html.erb
+        format.json { render json: @message }
+      end
     end
   end
 
