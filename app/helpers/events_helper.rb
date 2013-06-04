43eceb01cb1522b8e6e44adc7edd060ff9b9dfa9
Yee <yeekiwai@gmail.com>
2013-06-04 15:45:11 -0500

    Sort categories by putting asses with 0 users at the bottom

--- app/helpers/events_helper.rb
+++ app/helpers/events_helper.rb
@@ -1,2 +1,20 @@
 module EventsHelper
+  def translateAssTypeToVerb(kind)
+    case kind
+      when "Industry"
+        "are in "
+      when "Company"
+        "work(ed) for "
+      when "Location"
+        "live(d) in "
+      when "School"
+        "went to "
+      when "Language"
+        "know "
+      when "all"
+        "may or may not be like you, but still might be "
+      else 
+        "are in "
+    end
+  end
 end
